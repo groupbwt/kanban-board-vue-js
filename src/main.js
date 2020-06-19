6c5e624282bb570d699f99d3d83b9f943d3f2ad6
Vitaliy Belyy <belyy_vv@groupbwt.com>
2020-06-19 11:31:10 +0300

    feat: update project structure

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import i18n from './plugins/i18n'
 
+// Import app custom styles
+import './assets/sass/app.scss'
+
 Vue.config.productionTip = false
 
 new Vue({
