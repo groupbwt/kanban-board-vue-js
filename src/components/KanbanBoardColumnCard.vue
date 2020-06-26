ba1c91105b80527aebb1a56d5d3df40809d4c99b
Vitaliy Belyy <belyy_vv@groupbwt.com>
2020-06-26 10:19:36 +0300

    feat: add card component

---
+++ src/components/KanbanBoardColumnCard.vue
@@ -0,0 +1,101 @@
+<template>
+  <li class="kanban-board__card-wrapper">
+    <ValidationProvider
+      v-if="editMode"
+      ref="editForm"
+      name="card"
+      mode="passive"
+      rules="required"
+      v-slot="{ errors, failed }"
+    >
+      <input
+        ref="editField"
+        v-model="formData.title"
+        type="text"
+        :placeholder="$t('placeholders.card')"
+        :class="['kanban-board__edit-input', {'is-invalid': failed}]"
+        @keyup.enter="submitEditForm"
+      >
+      <div class="invalid-feedback">{{ errors[0] }}</div>
+    </ValidationProvider>
+
+    <div v-else class="kanban-board__card">
+      <p>{{ title }}</p>
+    </div>
+
+    <div class="kanban-board__card-actions">
+      <button class="kanban-board__icon-button" @click="toggleEditForm">
+        <img src="../assets/static/icons/pencil.svg" alt="">
+      </button>
+      <button class="kanban-board__icon-button" @click="deleteCard">
+        <img src="../assets/static/icons/delete.svg" alt="">
+      </button>
+    </div>
+  </li>
+</template>
+
+<script>
+import { ValidationProvider } from "vee-validate";
+
+export default {
+  name: 'KanbanBoardColumnCard',
+
+  components: {
+    ValidationProvider
+  },
+
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  },
+
+  data() {
+    return {
+      editMode: false,
+      formData: {
+        title: ''
+      },
+    }
+  },
+
+  methods: {
+    toggleEditForm() {
+      this.editMode ? this.disableEditForm() : this.enableEditForm();
+    },
+
+    enableEditForm() {
+      this.formData.title = this.title;
+      this.editMode = true;
+      this.$nextTick(() => {
+        this.$refs.editField.focus();
+      });
+    },
+
+    disableEditForm() {
+      this.editMode = false;
+      this.resetForm();
+    },
+
+    async submitEditForm() {
+      const validationResult = await this.$refs.editForm.validate();
+
+      if (validationResult.valid) {
+        this.$emit('update-card', this.formData.title);
+        this.editMode = false;
+        this.resetForm();
+      }
+    },
+
+    resetForm() {
+      this.formData.title = '';
+      this.$refs.editForm.reset();
+    },
+
+    deleteCard() {
+      this.$emit('delete-card');
+    }
+  }
+}
+</script>
