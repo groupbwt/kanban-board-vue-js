5e786466be3380ece30f465b7a632b3cc21e8bd7
Vitaliy Belyy <belyy_vv@groupbwt.com>
2020-06-29 15:54:00 +0300

    feat: install and configure stylelint

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,8 +6,8 @@ module.exports = {
   extends: [
     'plugin:vue/recommended',
     'eslint:recommended',
-    'prettier/vue',
     'plugin:prettier/recommended',
+    'prettier/vue',
   ],
   rules: {
     'vue/component-name-in-template-casing': ['error', 'PascalCase'],
